20cab4c4d4cee29617de3a43ad14e51ce979d193
Ariel Lobos <ariel@atariki.com>
2021-05-14 10:50:01 +0000

    Add app module settings for TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,33 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { ProductsService } from './products/products.service';
 import { ProductsController } from './products/products.controller';
 import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
-  controllers: [UsersController, ProductsController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: 'pguser',
+      password: 'pguser',
+      database: 'pgdb',
+      entities: [__dirname + '/**/*.entity.{ts,js}'],
+      synchronize: true,
+      logging: false,
+    }),
+    UsersModule,
+    ProductsModule,
+    AuthModule
+  ],
+  controllers: [
+    UsersController,
+    ProductsController
+  ],
   providers: [ProductsService],
 })
-export class AppModule {}
+export class AppModule { }
